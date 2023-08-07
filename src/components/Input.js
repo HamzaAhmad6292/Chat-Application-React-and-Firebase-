fc278ae944e357df0b28e007a87a15ae4d629827
Muhammad Hamza Ahmad <l216292@lhr.nu.edu.pk>
2023-08-07 15:41:03 +0500

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -83,21 +83,15 @@ const Input = () => {
         placeholder="  Type something..."
         onChange={(e) => setText(e.target.value)}
         value={text}
+        onKeyDown={handleKey}
       />
       <div className="send">
         <img src={Attach} alt="" />
-        <input
-          type="file"
-          style={{ display: "none" }}
-          id="file"
-          onChange={(e) => setImg(e.target.files[0])}
-        />
+
         <label htmlFor="file">
           <img src={Img} alt="" />
         </label>
-        <button onKeyDown={handleKey} onClick={handleSend}>
-          Send
-        </button>
+        <button onClick={handleSend}>Send</button>
       </div>
     </div>
   );
